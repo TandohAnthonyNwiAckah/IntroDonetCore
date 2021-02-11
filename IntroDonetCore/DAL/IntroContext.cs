017db9e5db8d7db6120acb527e71a41bc9e4b6fc
TANACOM <tanamoinc@gmail.com>
2021-02-11 19:25:15 +0000

    commit create Database using entitiy framework core.

---
+++ IntroDonetCore/DAL/IntroContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IntroDonetCore.DAL
+{
+    public class IntroContext: DbContext
+    {
+
+
+        public IntroContext(DbContextOptions options)
+            :base(options)
+        {
+
+
+        }
+
+
+
+
+
+    }
+}
--- IntroDonetCore/Startup.cs
+++ IntroDonetCore/Startup.cs
@@ -1,6 +1,9 @@
+using IntroDonetCore.DAL;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
@@ -12,10 +15,30 @@ namespace IntroDonetCore
 {
     public class Startup
     {
+
+        public Startup(IConfiguration config) {
+
+            Configuration = config;
+        }
+
+
+        public IConfiguration Configuration { get; set; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+
+            services.AddDbContext<IntroContext>(options =>
+            {
+
+                options.UseSqlServer(Configuration.GetConnectionString("Default"));
+
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
